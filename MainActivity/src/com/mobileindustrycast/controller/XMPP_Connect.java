2c39edf59f022b363215d1715f888e32c8fbe7dd
Borisbalde <malkoy@yandex.ru>
2013-02-12 07:04:09 -0800

    Send/Connect fix
    Now connect and send features work, need to figure out how to pull data
    from server. Possibly we need to resolve some connect isssues.

--- MainActivity/src/com/mobileindustrycast/controller/XMPP_Connect.java
+++ MainActivity/src/com/mobileindustrycast/controller/XMPP_Connect.java
@@ -1,12 +1,13 @@
 package com.mobileindustrycast.controller;
 
+
 import org.jivesoftware.smack.ConnectionConfiguration;
 import org.jivesoftware.smack.MessageListener;
-import org.jivesoftware.smack.SASLAuthentication;
 import org.jivesoftware.smack.XMPPConnection;
 import org.jivesoftware.smack.XMPPException;
 import org.jivesoftware.smack.Chat;
 import org.jivesoftware.smack.packet.Message;
+
 
 
 /**
@@ -14,54 +15,40 @@ import org.jivesoftware.smack.packet.Message;
  */
 public class XMPP_Connect implements MessageListener {
     //for final server use
-//	private static final int PORT_NUMBER = 5222 ;
-//	private static final String HOST = "openfire.orchestrate";
-//	private static final String SERVICE = "jabber.con";
+	private static final String HOST = "192.168.1.116";//"http://velington-pc"
+	private static final int PORT_NUMBER = 5222 ;
+	private static final String SERVICE = "conference.velington-pc";
+	private static final String RECIPIENT = "industrycast";
 	
 	XMPPConnection connection;
-
-	/**
-	 * Prepares a XMPP client and sends a message
-	 * @param m
-	 */
-	public void conectXMPP(String message, String sendTo)
-	{
-		
-		XMPP_Connect xmpp = new XMPP_Connect();
-		try{
-			xmpp.login("Andrey", "123");	
-		}catch (XMPPException ex){
-			
-		}//To be developed. Proposing Pop-up window "Cannot connect to the server"
-		 
-		try {
-			xmpp.sendMessage(message ,sendTo); // (message, userTosent)
-		} catch (XMPPException e) {
-			e.printStackTrace();
-		}
-
-		 xmpp.disconnect();
-	}
 	
-	 
+    
     public void login(String userName, String password) throws XMPPException {
-    	//ConnectionConfiguration config = new ConnectionConfiguration("http://velington-pc", 5222,"conference.velington-pc");
-    	//ConnectionConfiguration config = new ConnectionConfiguration(HOST ,PORT_NUMBER, SERVICE);
-    	ConnectionConfiguration config = new ConnectionConfiguration("jabber.org", 5222, "jabber.org");
+    	
+    	System.out.println("XMPP_Connect class' function 'login' started");
+    	ConnectionConfiguration config = new ConnectionConfiguration(HOST ,PORT_NUMBER, SERVICE);
+    	//config.setSASLAuthenticationEnabled(true);
+    	System.out.println("XMPP_Connect class' function 'login' configuration compleate");
+    	
         connection = new XMPPConnection(config);
- 
+
         connection.connect();
-        SASLAuthentication.supportSASLMechanism("PLAIN", 0);
+        System.out.println("connection to the server established");
+ 
         connection.login(userName, password);
+        System.out.println("XMPP_Connect class' function 'login' ended");
     }
  
-    public void sendMessage(String message, String to) throws XMPPException {
-        Chat chat = connection.getChatManager().createChat(to, this);
-        chat.sendMessage(message);
+    public void sendMessage(String message_body) throws XMPPException {
+    	System.out.println("XMPP_Connect class' function 'sendMessage' started");
+    	Message newMessage = new Message(RECIPIENT, Message.Type.groupchat);
+    	newMessage.setBody(message_body);
+    	connection.sendPacket(newMessage);
+        System.out.println("XMPP_Connect class' function 'sendMessage' ended");
     }
     
     public void processMessage(Chat chat, Message message) {
-        if (message.getType() == Message.Type.chat) {
+        if (message.getType() == Message.Type.groupchat) {
             System.out.println(chat.getParticipant() + " says: " + message.getBody());
             
 
--- MainActivity/src/com/mobileindustrycast/view/MainActivity.java
+++ MainActivity/src/com/mobileindustrycast/view/MainActivity.java
@@ -1,5 +1,7 @@
 package com.mobileindustrycast.view;
 
+import org.jivesoftware.smack.XMPPException;
+
 import com.example.mobileindustrycast.R;
 import com.mobileindustrycast.controller.XMPP_Connect;
 
@@ -12,12 +14,12 @@ import android.widget.EditText;
 
 public class MainActivity extends Activity  {
 
-	XMPP_Connect xmppchat = new XMPP_Connect();
-	
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
     }
 
     @Override
@@ -27,18 +29,48 @@ public class MainActivity extends Activity  {
         return true;
     }
     
-	public void clickListener(View v) {
+	public void sendMessage(View v) {
+		
 		switch (v.getId()) {
-		//Here we should have a request sending to XMPP server
+		
 		case R.id.sendBtn:
-			String message = findViewById(R.id.messageTxt).toString();
-			String sendTo = "dahernandez.89@gmail.com";
-	    	xmppchat.conectXMPP(message,sendTo);
-	    	
-	    	EditText chatLog = (EditText) findViewById(R.id.chatBox);
-	    	
-	    	chatLog.setText(chatLog.getText().append("\n").append(message));
 			
+			//Here we should have a request sending to XMPP server
+			System.out.println("Button pressed event occured");
+			
+						
+			new Thread(new Runnable(){
+				public void run()
+				{
+					String message = ((EditText)findViewById(R.id.messageTxt)).getText().toString();
+					XMPP_Connect xmpp = new XMPP_Connect();
+					System.out.println("Connection successfully established");
+					try
+					{
+						System.out.println(message);
+						xmpp.login("andrey", "12345");
+					}
+					catch (XMPPException ex)
+					{
+						System.out.println("Unable to log in with credinals provided");
+					}//To be developed. Proposing Pop-up window "Cannot connect to the server"
+			 
+					try 
+					{
+						xmpp.sendMessage(message); // (message, userTosent)
+					} 
+					catch (XMPPException e) 
+					{
+						System.out.println("Sending message failed");
+					}
+
+					xmpp.disconnect();
+				}
+			}).start();
+	    	//EditText chatLog = (EditText) findViewById(R.id.chatBox);
+	    	
+	    	//chatLog.setText(chatLog.getText().append("\n").append(message));
+	    	System.out.println("Error I passed the action");
 			break;
 		case R.id.broadcastBtn:
 			
